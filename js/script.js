f23e2bf25adf84b0a973611bd91963e4c27b30df
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2023-04-08 17:35:43 +0300

    Invalidation specified No falsy output

--- js/script.js
+++ js/script.js
@@ -28,69 +28,76 @@ function handleEnterPress(event) {
 	}
 }
 
+function invalidationError() {
+	const fields = [
+		{ input: dayInput, label: dayLabel, index: 0, min: 1, max: 31 },
+		{ input: monthInput, label: monthLabel, index: 1, min: 1, max: 12 },
+		{
+			input: yearInput,
+			label: yearLabel,
+			index: 2,
+			min: 1900,
+			max: currentYear.getFullYear(),
+		},
+	];
+
+	fields.forEach((field) => {
+		const input = field.input;
+		const label = field.label;
+		const index = field.index;
+		const min = field.min;
+		const max = field.max;
+		const value = input.value;
+
+		if (value == "" || value > max || value < min) {
+			input.style.color = "var(--light-red)";
+			if (value == "") {
+				invalidation[index].innerHTML = "Field is empty!";
+			} else if (value > max) {
+				invalidation[index].innerHTML =
+					"Value is above<br>current date!";
+			} else if (value < min) {
+				invalidation[index].innerHTML = "Value is under<br>valid date!";
+			}
+			invalidation[index].style.display = "block";
+			label.style.color = "var(--light-red)";
+			label.style.opacity = "1";
+			return false; // if input is invalid return true for invalidation
+		} else {
+			input.style.color = "var(--Off-black)";
+			invalidation[index].style.display = "none";
+			label.style.color = "var(--Off-black)";
+			label.style.opacity = "0.5";
+		}
+	});
+}
+
 function calculateAge() {
 	// Get user input values
 	const day = document.getElementById("day").value;
 	const month = document.getElementById("month").value;
 	const year = document.getElementById("year").value;
 
-	if (day == "" || day > 31 || day < 1) {
-		dayInput.style.color = "var(--light-red)";
-		invalidation[0].style.display = "block";
-		dayLabel.style.color = "var(--light-red)";
-		dayLabel.style.opacity = "1";
-		return;
-	} else {
-		dayInput.style.color = "var(--Off-black)";
-		invalidation[0].style.display = "none";
-		dayLabel.style.color = "var(--Off-black)";
-		dayLabel.style.opacity = "0.5";
+	if (invalidationError()) {
+		// Create Date object from user input values
+		const birthDate = new Date(year, month - 1, day);
+
+		// Calculate age in milliseconds
+		const ageInMs = Date.now() - birthDate.getTime();
+
+		// Calculate age in years, months, and days
+		const ageInYears = Math.floor(ageInMs / MS_PER_YEAR);
+		// MS_PER_YEAR = 1000ms * 60s * 60min * 24hrs * 365.25 days
+		const ageInMonths = Math.floor((ageInMs % MS_PER_YEAR) / 2629746000);
+		// 2629746000 = 1000ms * 60s * 60min * 24hrs * 30.44 days
+		const ageInDays = Math.floor((ageInMs % 2629746000) / 86400000);
+		// 86400000 = 1000ms * 60s * 60min * 24hrs
+
+		// Animate output fields with age values
+		animateOutput("years-output", ageInYears);
+		animateOutput("months-output", ageInMonths);
+		animateOutput("days-output", ageInDays);
 	}
-
-	if (month == "" || month > 12 || month < 1) {
-		monthInput.style.color = "var(--light-red)";
-		invalidation[1].style.display = "block";
-		monthLabel.style.color = "var(--light-red)";
-		monthLabel.style.opacity = "1";
-		return;
-	} else {
-		monthInput.style.color = "var(--Off-black)";
-		invalidation[1].style.display = "none";
-		monthLabel.style.color = "var(--Off-black)";
-		monthLabel.style.opacity = "0.5";
-	}
-
-	if (year == "" || year > currentYear.getFullYear()) {
-		yearInput.style.color = "var(--light-red)";
-		invalidation[2].style.display = "block";
-		yearLabel.style.color = "var(--light-red)";
-		yearLabel.style.opacity = "1";
-		return;
-	} else {
-		yearInput.style.color = "var(--Off-black)";
-		invalidation[2].style.display = "none";
-		yearLabel.style.color = "var(--Off-black)";
-		yearLabel.style.opacity = "0.5";
-	}
-
-	// Create Date object from user input values
-	const birthDate = new Date(year, month - 1, day);
-
-	// Calculate age in milliseconds
-	const ageInMs = Date.now() - birthDate.getTime();
-
-	// Calculate age in years, months, and days
-	const ageInYears = Math.floor(ageInMs / MS_PER_YEAR);
-	// MS_PER_YEAR = 1000ms * 60s * 60min * 24hrs * 365.25 days
-	const ageInMonths = Math.floor((ageInMs % MS_PER_YEAR) / 2629746000);
-	// 2629746000 = 1000ms * 60s * 60min * 24hrs * 30.44 days
-	const ageInDays = Math.floor((ageInMs % 2629746000) / 86400000);
-	// 86400000 = 1000ms * 60s * 60min * 24hrs
-
-	// Animate output fields with age values
-	animateOutput("years-output", ageInYears);
-	animateOutput("months-output", ageInMonths);
-	animateOutput("days-output", ageInDays);
 }
 
 function animateOutput(outputId, value) {
